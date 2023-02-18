5b7bed802ba6124c271209c3665809dd9bcf017d
Silvi887 <106423995+Silvi887@users.noreply.github.com>
2023-02-18 13:42:55 +0200

    KinderGarten

---
+++ 03KinderGarten/Child.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Child
+    {
+        public Child(string firstName,string lastName, int age,string parentName, string contactNumber)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.ParentName = parentName;
+            this.ContactNumber = contactNumber;
+        }
+        
+        public string FirstName { get; set; }
+        public string 	LastName { get; set; }
+        public int	Age { get; set; }
+        public string ParentName { get; set; }
+        public string ContactNumber { get; set; }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Child: {FirstName} {LastName}, Age: {Age}, Contact info: {ParentName} - {ContactNumber}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 03KinderGarten/Kindergarten.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public Kindergarten(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            Registry = new List<Child>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public List<Child> Registry { get; set; }
+        public int  ChildrenCount{get{return  Registry.Count; } }
+
+        public bool AddChild(Child child)
+        {
+            if (Registry.Count < Capacity)
+            {
+                Registry.Add(child);
+                return true;
+            }
+            return false;
+
+        }
+
+        public bool RemoveChild(string childFullName)
+        {
+            string[] SeparatedName = childFullName.Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string FirstName = SeparatedName[0];
+            string LastName= SeparatedName[1];
+
+            Child ChildForRemove =
+                Registry.FirstOrDefault(c => c.FirstName == FirstName && c.LastName == LastName);
+            if (ChildForRemove != null)
+            {
+                Registry.Remove(ChildForRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Child GetChild(string childFullName)
+        {
+            string[] SeparatedName = childFullName.Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string FirstName = SeparatedName[0];
+            string LastName = SeparatedName[1];
+
+            Child ChildWithNAmes =
+                Registry.FirstOrDefault(c => c.FirstName == FirstName && c.LastName == LastName);
+
+            if (ChildWithNAmes== null)
+            {
+                return null;
+            }
+            return ChildWithNAmes;
+        
+        }
+
+        public string RegistryReport()
+        {
+            StringBuilder sb= new StringBuilder();
+            sb.AppendLine($"Registered children in {Name}:");
+
+            List<Child> SortedChild = Registry.
+                OrderByDescending(c => c.Age).ThenBy(c => c.LastName).
+                ThenBy(ch => ch.FirstName).ToList();
+
+            foreach (var child in SortedChild)
+            {
+                sb.AppendLine($"{child.ToString()}");
+            }
+
+            return sb.ToString().Trim() ;
+
+        }
+
+
+
+    }
+}
